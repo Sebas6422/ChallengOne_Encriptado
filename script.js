101c9f902d01d3dead626512ede70c023b6c4b35
Sebas6422 <ansebastian.6422@gmail.com>
2023-07-10 09:07:10 -0500

    Se agrega el proyecto del encriptado Alura

---
+++ script.js
@@ -0,0 +1,57 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje-area");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+function btnDesincriptar(){
+    const textDesincriptado = desencriptar(textArea.value);
+    mensaje.value = textDesincriptado;
+    textArea = "";
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+    }
+    return stringEncriptada
+}
+
+function desencriptar(stringDesencriptar){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptar = stringDesencriptar.toLowerCase();
+    
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptar.includes(matrizCodigo[i][1])){
+            stringDesencriptar = stringDesencriptar.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
+    }
+
+    return stringDesencriptar
+}
+
+function copiarTexto(){
+    const button = document.querySelector(".btn-copiar");
+
+    mensaje.focus();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
+}
+
